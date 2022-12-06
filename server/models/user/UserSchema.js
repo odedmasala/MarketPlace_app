c3ccd81de1f29d0122f09527a79340f1774cb1eb
odedmasala <odedmasala6@gmail.com>
2022-12-06 16:18:49 +0200

    update userschema

--- server/models/user/UserSchema.js
+++ server/models/user/UserSchema.js
@@ -1,20 +1,27 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema =new mongoose.Schema({
-    _id: mongoose.Types.ObjectId(),
-    image:String,
-    firstName:String,
-    lastName:String,
-    email:String,
-    phone:String,
-    loginCode:String,
-    phone2:String,
-    password:String,
-    storeId:String,
-    address:String,
-    paymentBillInfo:String,
-    authToken:String
-})
+const UserSchema = new mongoose.Schema({
+  _id: mongoose.Types.ObjectId(),
+  image: {
+    type: String,
+    required: true,
+  },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true, min: 8, max: 50 },
+  phone: { type: String, required: true, min: 9, unique: true },
+  loginCode: String,
+  meager: {
+    type: Boolean,
+    default: null,
+  },
+  phone2: { type: String, min: 9 },
+  password: { type: String, required: true },
+  storeId: mongoose.Types.ObjectId(),
+  address: { type: Object, required: true },
+  paymentBillInfo: String,
+  authToken: String,
+});
 
-const model = mongoose.model("users",UserSchema)
-module.exports = model
+const model = mongoose.model("users", UserSchema);
+module.exports = model;
