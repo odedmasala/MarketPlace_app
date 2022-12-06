50b40c5777abe85609247736ff3aef85c9ddefb8
odedmasala <odedmasala6@gmail.com>
2022-12-07 00:38:58 +0200

    set-on server.js crete local connect to DB

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("The DB login was successful");
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,32 @@
-const express = require("express")
-const dotenv = require("dotenv")
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { handleError } = require("./utils/errorSetting");
+const app = express();
+
+// CONNECT TO DATABASE
+const connectDB = require("./config/config");
+// /* CONFIGURATIONS */
+
+
+/* ROUTES */
+app.use("/api/test", (req, res) => {
+    res.status(200).json("The server is up and running");
+});
+
+
+/* CATCH ERROR */
+app.use(handleError);
+
+/* DISCONNECTED MASSAGE FROM THE DATABASE */
+mongoose.connection.on("disconnected", () => {
+  console.log("mongo DB disconnected");
+});
+
+
+/* CREATE A LISTEN PORT AND SETON THE SERVER */
+const PORT = process.env.PORT || 6001;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`app listen http://localhost:${PORT}`);
+});
