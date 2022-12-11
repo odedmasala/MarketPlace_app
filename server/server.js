133a939b9c61f9492862706fd54dbfa29f756863
odedmasala <odedmasala6@gmail.com>
2022-12-11 18:55:59 +0200

    update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const helmet = require("helmet")
 const cors = require("cors")
+// const departmentRoute = require("./routes/department/department")
+// const productRoute = require("./routes/product/product")
+// const receiptRoute = require("./routes/receipt/receipt")
+// const sectionRoute = require("./routes/section/section")
+// const storeRoute = require("./routes/store/store")
+// const storeManagerRoute = require("./routes/storeManager/storeManager")
+// const userRoute = require("./routes/user/user")
+
 // CONNECT TO DATABASE
 const connectDB = require("./config/config");
 
@@ -18,6 +26,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 /* ROUTES */
 app.use("/api/test", (req, res) => res.status(200).json("The server is up and running"));
+// app.use("api/department",departmentRoute)
+// app.use("api/product",productRoute)
+// app.use("api/receipt",receiptRoute)
+// app.use("api/section",sectionRoute)
+// app.use("api/store",storeRoute)
+// app.use("api/storeManager",storeManagerRoute)
+// app.use("api/user",userRoute)
 
 
 /* CATCH ERROR */
