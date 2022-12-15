3ec60ef505172e73945986e5fa4c1ceff419df79
odedmasala <odedmasala6@gmail.com>
2022-12-15 10:24:46 +0200

    update body parser

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ const connectDB = require('./config/config');
 // /* CONFIGURATIONS */
 app.use(cors());
 app.use(helmet());
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 /* ROUTES */
 app.use("/api/test", (req, res) => res.status(200).json("The server is up and running"));
