92302321b45e717dd60a627720eb0e430f97d5f0
odedmasala <odedmasala6@gmail.com>
2022-12-19 21:40:56 +0200

    update new file data js

---
+++ server/data/section.js
@@ -0,0 +1,70 @@
+[
+    {
+      name: "פירות הדר",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "פירות (ללא הדר)",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "פירות טרופים",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "ירקות",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "כלי נשיפה",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "גיטרות וכלי מיתר",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "תופים וכלי הקשה",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "קלידים ופסנתרים",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "בישום יוקרה",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "טיפוח",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "איפור",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "טיפוח שיער מקצועי",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "גילוח והסרת שיער",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "יין ובירה",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "וודקה",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "וויסקי",
+      createdBy: "Jacob admin"
+    },
+    {
+      name: "טקילה",
+      createdBy: "Jacob admin"
+    }
+  ]
