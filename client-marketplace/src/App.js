41f5a7d0cab20c4607f46211cf2dafe5c7751639
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-19 21:39:18 +0200

    added components header and changed homepage bg picture + moved assrts folder from public to src

--- client-marketplace/src/App.js
+++ client-marketplace/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './css/style.css';
 import Footer from './components/footer/Footer';
 import NavBar from './components/navbar/NavBar';
+import HeaderStore from './components/header/HeaderStore';
+import Header from './components/header/Header';
 import HomePage from './components/homePage/HomePage';
 import SuccessPayment from './pages/successPayment/SuccessPayment';
+import Cart from './components/cart/Cart';
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <HomePage/>
-      <SuccessPayment/>
+      <Header />
+      {/* <HeaderStore /> */}
+      {/* <HomePage /> */}
+      <Cart />
+      {/* <SuccessPayment/> */}
       <Footer />
     </div>
   );
