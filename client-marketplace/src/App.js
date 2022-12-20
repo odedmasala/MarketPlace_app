f246fc478f0f8da02515667b442a7294b206a6fc
shelly <shely2111@gmail.com>
2022-12-20 13:08:46 +0200

    adding routers to app.js and an orders page to profile

--- client-marketplace/src/App.js
+++ client-marketplace/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './css/style.css';
-import Footer from './components/footer/Footer';
-import NavBar from './components/navbar/NavBar';
-import HomePage from './components/homePage/HomePage';
-import SuccessPayment from './pages/successPayment/SuccessPayment';
+import React from "react";
+import "./css/style.css";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
+import HomePage from "./components/homePage/HomePage";
+import SuccessPayment from "./pages/successPayment/SuccessPayment";
+import ProfilePage from "./pages/profile/ProfilePage";
+import MyOrders from "./pages/profile/MyOrders";
+import Product from "./components/product/Product";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-slate-100">
       <NavBar />
-      <HomePage/>
-      <SuccessPayment/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />}>
+          <Route path="/profile/myOrders" element={<MyOrders />} />
+        </Route>
+        <Route path="/successPage" element={<SuccessPayment />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
       <Footer />
     </div>
   );
--- client-marketplace/src/pages/successPayment/SuccessPayment.jsx
+++ client-marketplace/src/pages/successPayment/SuccessPayment.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import {FiCheckCircle} from 'react-icons/fi'
-import {CiFaceSmile} from 'react-icons/ci'
-import { Button } from 'flowbite-react'
+import React from "react";
+import { FiCheckCircle } from "react-icons/fi";
+import { CiFaceSmile } from "react-icons/ci";
+import { Button } from "flowbite-react";
 
 export default function SuccessPayment() {
   return (
-    <div className='w-5/12 border-black border-2 flex flex-col items-center my-36 m-auto text-center pt-5 h-[40rem]'>
-        <FiCheckCircle className='text-8xl text-[#0899A5] mb-10'/>
-        <div className='text-lg w-3/4 border-b-[1px] border-b-gray-400 mb-4 font-thin'>
-            <p className='text-4xl'>!תודה שהזמנת דרכנו</p>
-            <p className='flex items-center justify-center mb-4'><CiFaceSmile/>המשלוח כבר בדרך אלייך</p>
-        </div>
-        <div className='flex flex-col items-center justify-between h-[5rem]'>
+    <div className="w-5/12 bg-white flex flex-col items-center my-36 m-auto text-center pt-5 h-[40rem]">
+      <FiCheckCircle className="text-8xl text-[#0899A5] mb-10" />
+      <div className="text-lg w-3/4 border-b-[1px] border-b-gray-400 mb-4 font-thin">
+        <p className="text-4xl">!תודה שהזמנת דרכנו</p>
+        <p className="flex items-center justify-center mb-4">
+          <CiFaceSmile />
+          המשלוח כבר בדרך אלייך
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-between h-[5rem]">
         <p>מספר הזמנה: 111111111111111</p>
-        <Button className='w-3/4 bg-[#0899A5]'>חזרה לאתר</Button>
-        </div>
+        <Button outline={true} gradientDuoTone="greenToBlue">
+          Green to Blue
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
