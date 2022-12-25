4cdc0cad52a7b0b74fa02b5fe5a272d22979d1eb
odedmasala <odedmasala6@gmail.com>
2022-12-25 16:52:28 +0200

    update import export at app

---
+++ client-marketplace/src/pages/index.js
@@ -0,0 +1,10 @@
+export {default as CheckOut } from "./CheckOut/CheckOut"
+export {default as HomePage } from "./homePage/HomePage"
+export {default as SuccessPayment } from "./successPayment/SuccessPayment"
+export {default as MyOrders } from "./profile/MyOrders"
+export {default as PersonalDetails } from "./profile/PersonalDetails"
+export {default as ProfilePage } from "./profile/ProfilePage"
+export {default as StoreManager } from "./storeManager/StoreManager"
+export {default as Store } from "./storePage/Store"
+export {default as StoresListPage } from "./storeslist/StoresListPage"
+
