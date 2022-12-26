1e1c349fd3eb9f14b599d2f6d1beedddcb9decc5
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-26 23:43:54 +0200

    fixing mobile-nav bar

--- client-marketplace/src/pages/homePage/HomePage.jsx
+++ client-marketplace/src/pages/homePage/HomePage.jsx
@@ -1,29 +1,29 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Department from '../../components/department/Department';
+import FooterHomePage from '../../components/footer/FooterHomePage';
+import UserLocationPopUp from '../../components/userLocationPopup/UserLocationPopUp';
 
-import React from "react";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Department from "../../components/department/Department";
-import FooterHomePage from "../../components/footer/FooterHomePage";
-// import UserLocationPopUp from '../../components/userLocationPopup/UserLocationPopUp';
-import { useDispatch } from 'react-redux'
-import {getProducts} from "../../redux/storemanager/StoreManagerSlice"
+import * as reactRedux from 'react-redux';
+import { getProducts } from '../../redux/storemanager/StoreManagerSlice';
 
-import {ad} from "../../app/store"
+import { ad } from '../../app/store';
 
-import axios from "axios";
+import axios from 'axios';
 
 const HomePage = () => {
-  const dispatch = useDispatch()
+  const dispatch = reactRedux.useDispatch();
 
   const [allDepartments, setAllDepartments] = useState([]);
 
   const getDepartments = async () => {
-    const { data } = await axios.get("http://localhost:8001/api/department");
+    const { data } = await axios.get('http://localhost:8001/api/department');
     setAllDepartments(data);
   };
 
   useEffect(() => {
-    dispatch(getProducts())
+    dispatch(getProducts());
     getDepartments();
   }, [dispatch]);
 
