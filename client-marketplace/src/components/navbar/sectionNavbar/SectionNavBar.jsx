e9a85425d74ee237b2b74c7342f49c8e742c4593
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-12-28 01:00:22 +0200

    add

--- client-marketplace/src/components/navbar/sectionNavbar/SectionNavBar.jsx
+++ client-marketplace/src/components/navbar/sectionNavbar/SectionNavBar.jsx
@@ -5,7 +5,7 @@ export default function SectionNavBar({ categoryFilter, sections }) {
     <div className="text-center">
       {sections.map((element, index) => (
         <button
-          className="border-r-4 p-2 md:p-5 bg-white hover:bg-gray-100"
+          className="border-r-4 p-2 md:py-3 md:px-3 hover:bg-gray-100"
           key={index}
           onClick={() => {
             categoryFilter(element._id);
