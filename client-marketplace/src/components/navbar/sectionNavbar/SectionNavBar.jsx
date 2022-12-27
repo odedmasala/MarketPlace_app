129ee97b91321ee46b0910155ad160f009106f71
David Tassama <davidtassama464@gmail.com>
2022-12-28 01:24:30 +0200

    update

--- client-marketplace/src/components/navbar/sectionNavbar/SectionNavBar.jsx
+++ client-marketplace/src/components/navbar/sectionNavbar/SectionNavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function SectionNavBar({ categoryFilter, sections }) {
   return (
     <div className="text-center">
-      {sections.map((element, index) => (
+      {sections?.map((element, index) => (
         <button
           className="border-r-4 p-2 md:p-5 bg-white hover:bg-gray-100"
           key={index}
