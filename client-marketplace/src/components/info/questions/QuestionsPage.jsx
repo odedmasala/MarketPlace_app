78541bf2e02b996e4673867be1d54097b2e5d96e
David Tassama <davidtassama464@gmail.com>
2022-12-28 05:50:28 +0200

    update profile page

--- client-marketplace/src/components/info/questions/QuestionsPage.jsx
+++ client-marketplace/src/components/info/questions/QuestionsPage.jsx
@@ -1,40 +1,31 @@
-import React from 'react'
-import Dropdown from './Dropdown'
+import React from "react";
+import Dropdown from "./Dropdown";
 
 const QuestionsPage = () => {
-  const questions =[
+  const questions = [
     {
-      title:"חחכחכחכ",
-      info:"dvsvs sdv sdvc iugd kads fkus ;jksd;basd jhsvdh  vhzs nzkcjbv;s fdzsdb ubdsfsd fb sdnbskd fb"
+      title: " המשלוח לוקח יותר מדי זמן     ",
+      info: "הסיבות הנפוצות לאיחור במשלוחים הן עומס במסעדה או עומס עם משלוח וולט. אנו מתאימים אוטומטית את הערכות הזמן שלנו על סמך הזמנים בפועל.",
     },
     {
-      title:"חחכחכחכ",
-      info:"dvsvs sdv sdvc iugd kads fkus ;jksd;basd jhsvdh  vhzs nzkcjbv;s fdzsdb ubdsfsd fb sdnbskd fb"
+      title: "האפליקציה לא לקחה בחשבון את הזיכויים שלי      ",
+      info: "לאפליקציית iOS יש מסך אישור כחול שחוזר על עובדות ההזמנה הבסיסיות, כמו הסכום הכולל. אנו תמיד משתמשים בזיכויים שלך אם ההזמנה יכולה להשתמש בהם. חלק מהזיכויים תקפים להזמנות משלוח בלבד.",
     },
+
     {
-      title:"חחכחכחכ",
-      info:"dvsvs sdv sdvc iugd kads fkus ;jksd;basd jhsvdh  vhzs nzkcjbv;s fdzsdb ubdsfsd fb sdnbskd fb"
+      title: "חלק מהפריטים בהזמנה שלי חסרים או שגויים  ",
+      info: "צור קשר עם התמיכה שלנו ואנו נתקן את זה. לפעמים מסעדות או שליחים טועים. אנחנו עושים כל שביכולתנו כדי לתקן כל טעות אנוש",
     },
-    {
-      title:"חחכחכחכ",
-      info:"dvsvs sdv sdvc iugd kads fkus ;jksd;basd jhsvdh  vhzs nzkcjbv;s fdzsdb ubdsfsd fb sdnbskd fb"
-    },
-    {
-      title:"חחכחכחכ",
-      info:"dvsvs sdv sdvc iugd kads fkus ;jksd;basd jhsvdh  vhzs nzkcjbv;s fdzsdb ubdsfsd fb sdnbskd fb"
-    },
-  ]
+  ];
   return (
     <div className="py-6 px-14 text-end">
-    <h1 className="text-center text-4xl mb-14">שאלות נפוצות</h1>
-   
-   {
-    questions.map((question,i)=><Dropdown key={i} question={question}/>)
-   }
-   
-   
-  </div>
-  )
-}
+      <h1 className="text-center text-4xl mb-14">שאלות נפוצות</h1>
 
-export default QuestionsPage
+      {questions.map((question, i) => (
+        <Dropdown key={i} question={question} />
+      ))}
+    </div>
+  );
+};
+
+export default QuestionsPage;
